27806ea650fca9a299131fb01d3ed747a52b02bd
Taeyeon <lty0314@gmail.com>
2024-09-26 02:05:26 +0900

    feat: 레이아웃 및 메인 페이지 구현

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['img.icons8.com'], // 여기에 허용할 도메인 추가
+    },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,61 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="container mx-auto mt-10">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold">리그 오브 레전드 정보 앱</h1>
+        <p className="mt-4 text-gray-500">Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.</p>
+        <div className="flex flex-col justify-center gap-10 mt-5">
+          <Link className="flex flex-col justify-center items-center gap-5 text-amber-400" href="/champions">
+            <div className="relative w-[400px] h-[240px]">
+              <Image src="/images/aphelios.jpg" alt="Aphelios" width={400} height={400} />
+            </div>
+            <div className="flex flex-row items-center gap-2 bg-white px-2  text-black rounded-2xl font-medium w-48">
+              <Image
+                width="48"
+                height="48"
+                src="https://img.icons8.com/fluency/48/league-of-legends.png"
+                alt="league-of-legends"
+              />
+              챔피언 목록 보기
+            </div>
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-col justify-center gap-10 mt-5">
+          <Link className="flex flex-col justify-center items-center gap-5 text-amber-400" href="/champions">
+            <div className="relative w-[400px] h-[240px]">
+              <Image src="/images/yuumi.jpg" alt="yuumi" width={400} height={400} />
+            </div>
+            <div className="flex flex-row items-center gap-2 bg-white px-2  text-black rounded-2xl font-medium	w-48">
+              <Image
+                width="48"
+                height="48"
+                src="https://img.icons8.com/fluency/48/league-of-legends.png"
+                alt="league-of-legends"
+              />
+              금주 로테이션 확인
+            </div>
+          </Link>
+        </div>
+        <div className="flex flex-col justify-center gap-10 mt-5 font-medium	">
+          <Link className="flex flex-col justify-center items-center gap-5 text-amber-400" href="/champions">
+            <div className="relative w-[400px] h-[240px]">
+              <Image src="/images/teemo.png" alt="teemo" width={400} height={400} />
+            </div>
+            <div className="flex flex-row items-center gap-2 bg-white px-2  text-black rounded-2xl w-48">
+              <Image
+                width="48"
+                height="48"
+                src="https://img.icons8.com/fluency/48/league-of-legends.png"
+                alt="league-of-legends"
+              />
+              아이템 목록 보기
+            </div>
+          </Link>
+        </div>
+      </div>
+    </main>
   );
 }
