e7da90cdbea86e610c412ec9017439b7251ac1ea
Taeyeon <lty0314@gmail.com>
2024-10-07 23:12:28 +0900

    feat: 챔피언 로테이션 페이지 추가

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -1,4 +1,9 @@
-const page = () => {
-  return <div>page</div>;
+import RotationChampionPage from "./(components)/RotationChampionPage";
+
+export const metadata = {
+  title: "챔피언 로테이션 목록 - LOL DEX",
+  description: "리그 오브 레전드 챔피언 로테이션 목록"
 };
-export default page;
+export default function Page() {
+  return <RotationChampionPage />;
+}
