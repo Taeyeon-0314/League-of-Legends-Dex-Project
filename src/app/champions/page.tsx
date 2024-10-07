3b9ffe0f67bdb27639fd66107e2797eaf144e9e8
Taeyeon <lty0314@gmail.com>
2024-10-07 23:09:22 +0900

    feat: 아이템 목록 페이지에 카드 컴포넌트 추가

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -2,6 +2,7 @@ import { Champions, Versions } from "@/utils/serverApi";
 import Image from "next/image";
 import Link from "next/link";
 import { Champion } from "@/types/Champion";
+import Card from "@/components/Card";
 
 export const metadata = {
   title: "챔피언 목록 - LOL DEX",
@@ -10,10 +11,9 @@ export const metadata = {
 
 const ChampionsPage = async () => {
   let champions: Champion[] = [];
-  let version: string = "";
+  const version: string = await Versions();
 
   try {
-    version = await Versions();
     const championsData = await Champions(version);
 
     if ("error" in championsData) {
@@ -25,31 +25,18 @@ const ChampionsPage = async () => {
     console.error(error);
   }
 
+  const championItems = champions.map((champion) => ({
+    id: champion.id,
+    name: champion.name,
+    imageUrl: `${process.env.NEXT_RIOT_DDRAGON_URL}/cdn/${version}/img/champion/${champion.id}.png`,
+    linkUrl: `/champions/${champion.id}`,
+    description: champion.title
+  }));
+
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4 text-teal-800">챔피언 목록</h1>
-      <div className="grid grid-cols-4 gap-4 ">
-        {champions.length > 0 ? (
-          champions.map((champion) => (
-            <div className="border border-solid border-teal-800 rounded p-4 hover:shadow-lg" key={champion.id}>
-              <Link href={`/champions/${champion.id}`}>
-                <div className="flex justify-center">
-                  <Image
-                    src={`${process.env.NEXT_RIOT_DDRAGON_URL}/cdn/${version}/img/champion/${champion.id}.png`}
-                    alt={champion.name}
-                    width={100}
-                    height={100}
-                  />
-                </div>
-                <p className="mt-2 font-semibold text-teal-800">{champion.name}</p>
-                <p className="text-sm text-gray-500">{champion.title}</p>
-              </Link>
-            </div>
-          ))
-        ) : (
-          <p>챔피언 데이터를 가져올 수 없습니다.</p>
-        )}
-      </div>
+      <Card items={championItems} />
     </div>
   );
 };
