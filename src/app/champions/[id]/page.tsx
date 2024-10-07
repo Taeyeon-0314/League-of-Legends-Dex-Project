8090b1cde7ebd5b2baae4c942fb1c6fef36194cd
Taeyeon <lty0314@gmail.com>
2024-10-07 11:10:15 +0900

    feat: 챔피언 상세 페이지 추가
    챔피언 상세 페이지에 이미지, 스킬, 스킨 정보를 확인할 수 있게함

---
+++ src/app/champions/[id]/page.tsx
@@ -0,0 +1,83 @@
+import Image from "next/image";
+import { fetchChampionDetail } from "@/utils/serverApi";
+import { ChampionSkin, ChampionSpell } from "@/types/Champion";
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const champion = await fetchChampionDetail(params.id);
+
+  if (!champion) {
+    return { title: "챔피언을 찾을 수 없습니다" };
+  }
+
+  return {
+    title: `${champion.name} - LOL DEX`,
+    description: `${champion.name}의 상세 정보`
+  };
+}
+
+const ChampionDetailPage = async ({ params }: { params: { id: string } }) => {
+  const champion = await fetchChampionDetail(params.id);
+
+  if (!champion) {
+    return <div>챔피언을 찾을 수 없습니다!!!</div>;
+  }
+
+  const cleanDescription = (description: string) => {
+    return description
+      .replace(/<br\s*\/?>/gi, "\n")
+      .replace(/<\/?[^>]+(>|$)/g, "")
+      .replace(/<keywordMajor>|<\/keywordMajor>/g, "")
+      .replace(/<font[^>]*>|<\/font>/g, "");
+  };
+
+  return (
+    <div>
+      <h1 className="text-teal-800 text-5xl mb-4">{champion.name}</h1>
+      <h2 className="text-gray-600 text-2xl mb-4">{champion.title}</h2>
+      <div className="flex justify-center">
+        <Image src={champion.image} alt={champion.name} width={600} height={600} />
+      </div>
+      <p className="text-teal-800 mt-4 text-lg">{champion.description}</p>
+      <div className="mt-6">
+        <h1 className="text-teal-800 text-4xl mb-3">스탯</h1>
+        <li className="text-teal-800">기본 체력: {champion.stats.hp}</li>
+        <li className="text-teal-800">기본 마나: {champion.stats.mp}</li>
+        <li className="text-teal-800">기본 공격력: {champion.info.attack}</li>
+        <li className="text-teal-800">기본 방어력: {champion.info.defense}</li>
+        <li className="text-teal-800">기본 주문력: {champion.info.magic}</li>
+        <li className="text-teal-800">난이도: {champion.info.difficulty}</li>
+      </div>
+      <h1 className="text-teal-800 text-4xl mt-6 mb-3 ">스킬</h1>
+      <div className="grid grid-cols-4 gap-2">
+        {champion.spells.map((spell: ChampionSpell) => (
+          <div className="border border-solid border-teal-800" key={spell.id}>
+            <div className="flex justify-center">
+              <Image src={spell.image} alt={spell.name} width={100} height={100} />
+            </div>
+            <h2 className="text-teal-800 text-2xl mt-4 text-center">{spell.name}</h2>
+            <p className="text-teal-800 mt-5">{cleanDescription(spell.description)}</p>
+          </div>
+        ))}
+      </div>
+      <h1 className="text-teal-800 text-4xl mt-6 mb-3 ">스킨</h1>
+      <div className="grid grid-cols-4 gap-2 text-center">
+        {champion.skins.map((skin: ChampionSkin) => (
+          <div key={skin.id}>
+            <div className="flex justify-center">
+              <Image
+                src={`${process.env.NEXT_RIOT_DDRAGON_URL}/cdn/img/champion/splash/${champion.id}_${skin.num}.jpg`}
+                alt={skin.name}
+                width={200}
+                height={200}
+              />
+            </div>
+
+            <p className="text-teal-800 mt-2 text-lg">{skin.name === "default" ? "기본스킨" : skin.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ChampionDetailPage;
