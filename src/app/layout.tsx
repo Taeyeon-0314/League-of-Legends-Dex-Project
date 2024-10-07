544c3c30956166065f975ce948fe13f8502385cb
Taeyeon <lty0314@gmail.com>
2024-10-07 11:00:33 +0900

    style: 레이아웃 스타일 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
-import "./globals.css";
 import "./reset.css";
+import "./globals.css";
 import ClientHeader from "@/components/ClientHeader";
 
 export const metadata: Metadata = {
   title: "LOL DEX",
-  description: "League of Legends champion index",
+  description: "League of Legends champion",
   icons: {
     icon: "/images/icon.svg"
   }
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="ko">
       <body className="pt-[64px] pb-[64px]">
         <ClientHeader />
-        <main className="container mx-auto px-4 flex-grow pt-[64px]">{children}</main>
+        <main className="container mx-auto px-4 flex-grow pt-[30px]">{children}</main>
         <footer className="bg-teal-800 text-white p-4 fixed bottom-0 w-full">
           <div className="container mx-auto text-center text-white text-sm">© 2024 LOL DEX</div>
         </footer>
