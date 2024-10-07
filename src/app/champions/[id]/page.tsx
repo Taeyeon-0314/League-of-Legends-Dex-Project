8071b1134434c301023e22d78081f6b1ba816852
Taeyeon <lty0314@gmail.com>
2024-10-07 23:10:48 +0900

    feat: 아이템 목록 페이지 및 업데이트 챔피언 세부 정보 페이지에 카드 구성 요소 추가

--- src/app/champions/[id]/page.tsx
+++ src/app/champions/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { fetchChampionDetail } from "@/utils/serverApi";
 import { ChampionSkin, ChampionSpell } from "@/types/Champion";
+import Card from "@/components/Card";
 
 export async function generateMetadata({ params }: { params: { id: string } }) {
   const champion = await fetchChampionDetail(params.id);
@@ -30,6 +31,21 @@ const ChampionDetailPage = async ({ params }: { params: { id: string } }) => {
       .replace(/<font[^>]*>|<\/font>/g, "");
   };
 
+  const statsInfo = [
+    { label: "기본 체력", value: champion.stats.hp },
+    { label: "기본 마나/기력", value: champion.stats.mp },
+    { label: "기본 공격력/주문력", value: champion.stats.attackdamage },
+    { label: "기본 방어력", value: champion.stats.armor },
+    { label: "기본 주문력", value: champion.stats.spellblock }
+  ];
+
+  const skinItems = champion.skins.map((skin: ChampionSkin) => ({
+    id: skin.id,
+    name: skin.name === "default" ? "기본 스킨" : skin.name,
+    imageUrl: `${process.env.NEXT_RIOT_DDRAGON_URL}/cdn/img/champion/splash/${champion.id}_${skin.num}.jpg`,
+    linkUrl: `/champion/${champion.id}`
+  }));
+
   return (
     <div>
       <h1 className="text-teal-800 text-5xl mb-4">{champion.name}</h1>
@@ -40,12 +56,11 @@ const ChampionDetailPage = async ({ params }: { params: { id: string } }) => {
       <p className="text-teal-800 mt-4 text-lg">{champion.description}</p>
       <div className="mt-6">
         <h1 className="text-teal-800 text-4xl mb-3">스탯</h1>
-        <li className="text-teal-800">기본 체력: {champion.stats.hp}</li>
-        <li className="text-teal-800">기본 마나: {champion.stats.mp}</li>
-        <li className="text-teal-800">기본 공격력: {champion.info.attack}</li>
-        <li className="text-teal-800">기본 방어력: {champion.info.defense}</li>
-        <li className="text-teal-800">기본 주문력: {champion.info.magic}</li>
-        <li className="text-teal-800">난이도: {champion.info.difficulty}</li>
+        {statsInfo.map(({ label, value }) => (
+          <li key={label} className="text-teal-800">
+            {label}: {value}
+          </li>
+        ))}
       </div>
       <h1 className="text-teal-800 text-4xl mt-6 mb-3 ">스킬</h1>
       <div className="grid grid-cols-4 gap-2">
@@ -60,22 +75,7 @@ const ChampionDetailPage = async ({ params }: { params: { id: string } }) => {
         ))}
       </div>
       <h1 className="text-teal-800 text-4xl mt-6 mb-3 ">스킨</h1>
-      <div className="grid grid-cols-4 gap-2 text-center">
-        {champion.skins.map((skin: ChampionSkin) => (
-          <div key={skin.id}>
-            <div className="flex justify-center">
-              <Image
-                src={`${process.env.NEXT_RIOT_DDRAGON_URL}/cdn/img/champion/splash/${champion.id}_${skin.num}.jpg`}
-                alt={skin.name}
-                width={200}
-                height={200}
-              />
-            </div>
-
-            <p className="text-teal-800 mt-2 text-lg">{skin.name === "default" ? "기본스킨" : skin.name}</p>
-          </div>
-        ))}
-      </div>
+      <Card items={skinItems} />
     </div>
   );
 };
